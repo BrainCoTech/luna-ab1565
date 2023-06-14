9245eca6e54b0709b63c620fc03f0be253871185
gaoxiang89 <gaoxiang89ly@gmail.com>
2023-06-14 10:10:21 +0800

    disable hfp function

--- mcu/project/morpheus/apps/morpheus_design/GCC/feature_morpheus.mk
+++ mcu/project/morpheus/apps/morpheus_design/GCC/feature_morpheus.mk
@@ -93,8 +93,8 @@ MTK_BT_DUO_ENABLE = y
 # bt module enable
 MTK_BT_ENABLE                       = y
 MTK_BLE_ONLY_ENABLE                 = n
-MTK_BT_HFP_ENABLE                   = y
-MTK_BT_HSP_ENABLE                   = y
+MTK_BT_HFP_ENABLE                   = n
+MTK_BT_HSP_ENABLE                   = n
 MTK_BT_AVRCP_ENABLE                 = y
 MTK_BT_AVRCP_ENH_ENABLE             = y
 MTK_BT_A2DP_ENABLE                  = y
