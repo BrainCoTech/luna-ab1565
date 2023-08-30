d8fcb31cd396a1d7b39cd0163bf37663a9fc76cc
gaoxiang89 <gaoxiang89ly@gmail.com>
2023-08-30 15:30:34 +0800

    MSP001 提示音

--- tools/vp/vp_generated/convert_to_tts.py
+++ tools/vp/vp_generated/convert_to_tts.py
@@ -1,24 +1,18 @@
 import pandas as pd
-import pyttsx3
-from pydub import AudioSegment
+import os
+import subprocess
 
 # 获取excel表的数据
 df = pd.read_excel('luna_vp_table.xlsx')  # 更改为你的Excel表位置
-
-# 因为aspeak在Windows上有些问题，我们使用pyttsx3来替代它
-engine = pyttsx3.init()
 
 # 处理每一行数据
 for index, row in df.iterrows():
     filename = row[0]
     text = row[1]
-    
-    # 使用pyttsx3把字符转为wav文件
-    engine.save_to_file(text, filename + ".wav")
-    engine.runAndWait()
 
-    # 把wav文件转为mp3文件
-    sound = AudioSegment.from_wav(filename + ".wav")
-    sound = sound.set_channels(1)
-    sound = sound.set_frame_rate(48000)
-    sound.export(filename + ".mp3", format="mp3")
+
+    # create the command
+    command = f'./aspeak text "{text}" -o {filename}.mp3 -c mp3 -q=2 -v zh-CN-XiaoxiaoNeural'
+
+    # execute the command
+    subprocess.call(command, shell=True)
