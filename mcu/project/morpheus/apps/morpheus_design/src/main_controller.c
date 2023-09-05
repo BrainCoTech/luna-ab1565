e5760b20a0a0d7e07becd928078cd41b2fb45c42
gaoxiang <gaoxiang89ly@outlook.com>
2023-09-05 11:49:55 +0800

    enable 32KHz clock for nrf52840 and max30001

--- mcu/project/morpheus/apps/morpheus_design/src/main_controller.c
+++ mcu/project/morpheus/apps/morpheus_design/src/main_controller.c
@@ -21,9 +21,15 @@ log_create_module(MAIN_CONTR, PRINT_LEVEL_INFO);
 log_create_module(MUSIC_CONTR, PRINT_LEVEL_INFO);
 
 #define MAIN_POWEN_EN_PIN HAL_GPIO_9
-#define POWERKEY_PIN HAL_GPIO_8
+#define POWERKEY_PIN HAL_GPIO_10
+#define CLK_32K_EN_PIN HAL_GPIO_8
 
 void main_controller_gpio_init(void) {
+    hal_gpio_init(CLK_32K_EN_PIN);
+    hal_pinmux_set_function(CLK_32K_EN_PIN, 0);
+    hal_gpio_set_direction(CLK_32K_EN_PIN, HAL_GPIO_DIRECTION_OUTPUT);
+    hal_gpio_set_output(CLK_32K_EN_PIN, HAL_GPIO_DATA_HIGH);
+
     hal_gpio_init(MAIN_POWEN_EN_PIN);
     hal_pinmux_set_function(MAIN_POWEN_EN_PIN, 0);
     hal_gpio_set_direction(MAIN_POWEN_EN_PIN, HAL_GPIO_DIRECTION_OUTPUT);
